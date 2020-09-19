61d0a6bd7cdd5bfed0a995ed7f29f7aa5542cd14
K.Tsukimoto <t.29.wuq@gmail.com>
2020-09-19 09:12:13 +0000

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_5
+
+class Switch(app_manager.RyuApp):
+	OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
+	def __init__(self, *args, **kwargs):
+		super(Switch, self).__init__(*args, **kwargs)
+
