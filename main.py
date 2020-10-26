ace52afc43a3c293a162f68a2c0c08ee363bccca
K.Tsukimoto <t.29.wuq@gmail.com>
2020-10-26 08:22:32 +0000

    :rocket: auto commit: update openflow controller

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_5
 from ryu.lib.packet import ethernet
