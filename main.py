f8bb4b6d4127d93f209c5b7157a3dff1c09ab942
K.Tsukimoto <t.29.wuq@gmail.com>
2020-12-12 17:12:38 +0000

    :rocket: auto commit: update openflow controller

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ class Switch(app_manager.RyuApp):
 				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 80, ), instructions = permit, ))
 				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 443, ), instructions = permit, ))
 				datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(2),], ))
+			else:
+				datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(3),], ))
 
 	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
 	def event_FeaturesRequest(self, ev):
@@ -44,6 +46,4 @@ class Switch(app_manager.RyuApp):
 		parser = datapath.ofproto_parser
 		ofproto = datapath.ofproto
 		datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(), priority = 0, instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER),]),], ))
-
-
 
