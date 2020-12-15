34fb3d95e1cadd8919dfcb66c014733c7eaecfb4
K.Tsukimoto <t.29.wuq@gmail.com>
2020-12-15 19:04:48 +0000

    :rocket: auto commit: update openflow controller

--- main.py
+++ main.py
@@ -3,34 +3,17 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_5
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import udp
 from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from functions.is_auth import is_auth
+from ryu.lib.packet import ipv4
 
 class Switch(app_manager.RyuApp):
 	OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
 	def __init__(self, *args, **kwargs):
 		super(Switch, self).__init__(*args, **kwargs)
 		self.mac_to_port = {}
-
-	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-	def event_PacketIn(self, ev):
-		msg = ev.msg
-		datapath = msg.datapath
-		pkt = packet.Packet(msg.data)
-		eth = pkt.get_protocols(ethernet.ethernet)[0]
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
-		self.mac_to_port.setdefault(datapath.id, {})
-		self.mac_to_port[datapath.id][eth.src] = msg.match['in_port']
-		if eth.dst in self.mac_to_port[datapath.id]:
-			out_port = self.mac_to_port[datapath.id][eth.dst]
-		else:
-			out_port = ofproto.OFPP_FLOOD
-		if out_port != ofproto.OFPP_FLOOD:
-			datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(in_port = msg.match['in_port'], eth_dst = eth.dst, ), priority = 1, instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(out_port),]),], ))
-		if ofproto.OFP_NO_BUFFER == msg.buffer_id:
-			data = msg.data
-		datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(out_port),], data = data, ))
 
 	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
 	def event_FeaturesRequest(self, ev):
@@ -39,3 +22,25 @@ class Switch(app_manager.RyuApp):
 		parser = datapath.ofproto_parser
 		ofproto = datapath.ofproto
 		datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(), priority = 0, instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER),]),], ))
+
+	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+	def event_PacketIn(self, ev):
+		msg = ev.msg
+		pkt = packet.Packet(msg.data)
+		u = pkt.get_protocols(udp.udp)
+		eth = pkt.get_protocols(ethernet.ethernet)[0]
+		datapath = msg.datapath
+		parser = datapath.ofproto_parser
+		v4 = pkt.get_protocols(ipv4.ipv4)
+		if u.dst_port == 67:
+			if True != eth.src in self.mac_to_port:
+				self.mac_to_port[eth.src] = msg.match['in_port']
+				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(eth_dst = eth.src, ), instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(msg.match['in_port']),]),], ))
+			datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(1),], ))
+		else:
+			if is_auth(ip_address = v4.src, ):
+				permit = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(2),]),]
+				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 80, ), instructions = permit, ))
+				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 443, ), instructions = permit, ))
+				datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(2),], ))
+
