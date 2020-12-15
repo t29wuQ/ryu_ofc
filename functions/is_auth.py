66ab2b5e3f8d20cc9dcdae9dcb783056bc3e60b0
K.Tsukimoto <t.29.wuq@gmail.com>
2020-12-15 19:21:06 +0000

    :rocket: auto commit: update openflow controller

--- functions/is_auth.py
+++ functions/is_auth.py
@@ -3,5 +3,3 @@ import requests
 def is_auth(ip_address = ''):
     response = requests.get('http://localhost:5281/auth?ip='+ip_address)
     return response.json()['status']
-
-print(is_auth(ip_address = '172.29.0.1'))
--- main.py
+++ main.py
@@ -15,14 +15,6 @@ class Switch(app_manager.RyuApp):
 		super(Switch, self).__init__(*args, **kwargs)
 		self.mac_to_port = {}
 
-	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-	def event_FeaturesRequest(self, ev):
-		msg = ev.msg
-		datapath = msg.datapath
-		parser = datapath.ofproto_parser
-		ofproto = datapath.ofproto
-		datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(), priority = 0, instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER),]),], ))
-
 	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
 	def event_PacketIn(self, ev):
 		msg = ev.msg
@@ -43,4 +35,14 @@ class Switch(app_manager.RyuApp):
 				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 80, ), instructions = permit, ))
 				datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(ipv4_src = v4.src, tcp_dst = 443, ), instructions = permit, ))
 				datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(2),], ))
+			else:
+				datapath.send_msg(parser.OFPPacketOut(datapath=datapath, buffer_id = msg.buffer_id, match = parser.OFPMatch(in_port = msg.match['in_port'], ), actions = [parser.OFPActionOutput(3),], ))
+
+	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+	def event_FeaturesRequest(self, ev):
+		msg = ev.msg
+		datapath = msg.datapath
+		parser = datapath.ofproto_parser
+		ofproto = datapath.ofproto
+		datapath.send_msg(parser.OFPFlowMod(datapath=datapath, match = parser.OFPMatch(), priority = 0, instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER),]),], ))
 
